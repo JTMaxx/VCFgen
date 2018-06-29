0e014d6dbfb282742ed8d3888a93a865a1e6b527
JTMaxx <j.m.okrasa@gmail.com>
2018-06-29 09:02:09 +0200

    exemplary values works

--- src/main/java/Header.java
+++ src/main/java/Header.java
@@ -3,10 +3,10 @@ import java.util.List;
 public class Header {
     public void xx() {
         Reader reader = new Reader();
-        //List<String> Contacts = reader.readContacts();
+        List<String> Contacts = reader.readContacts();
         System.out.println("Put type of this field.");
-        for (int i = 0; i<3; i++) {
-            System.out.println("exemplary values:");
+        System.out.println("\nexemplary values:");
+        for (int i = 1; i<4; i++) { // i=1, a nie 0, bo to poprawka na 1 niewyswietlajacy sie wiersz
             System.out.println(reader.getFieldContent(Contacts,1, i));
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public final static int COLUMNS_NUMBER = 4;
 
     public static void main(String[] args) {
-        Reader reader = new Reader();
-        List<String> Contacts = reader.readContacts();
+        //Reader reader = new Reader();
 
         Header header = new Header();
 
